4f10ac4a5a5338180735e6cd490e27ece42f1bf8
Justin <justingcyeung@gmail.com>
2019-08-10 20:25:34 -0400

    add default values to schemas

--- models/blooper.js
+++ models/blooper.js
@@ -2,8 +2,8 @@ var mongoose = require("mongoose");
 
 // video schema setup
 var blooperSchema = new mongoose.Schema({
-    videoType: String,
-    name: String,
+    videoType: {type: String, default: "blooper"},
+    name: {type: String, default: "New Blooper"},
     // url: String,
     description: String,
     // views: Number
--- models/video.js
+++ models/video.js
@@ -1,14 +1,17 @@
 var mongoose = require("mongoose");
 
+var today = new Date();
+
 // video schema setup
 var videoSchema = new mongoose.Schema({
-    videoType: String,
-    name: String,
-    season: Number,
-    number: Number,
+    videoType: {type: String, default: "episode"},
+    name: {type: String, default: "New Episode"},
+    season: {type: Number, default: 1},
+    number: {type: Number, default: 1},
+    thumbnail: {type: String, default: "https://cdn.wallpapersafari.com/76/19/oTkiJr.jpg"},
     // url: String,
-    description: String,
-    uploaded: Object,
+    description: {type: String, default: "New Episode out today!"},
+    uploaded: {type: Object, default: today},
     // views: Number
     bloopers:[
         {
