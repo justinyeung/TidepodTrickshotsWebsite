94aa595462d3a903e3159fa8a8df94c7f39b7a8d
Justin <justingcyeung@gmail.com>
2019-08-20 23:43:29 -0400

    add environment variables to db/port

--- app.js
+++ app.js
@@ -22,13 +22,9 @@ User = require("./models/user");
 //use body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 // connect to mongoose database
-mongoose.connect("mongodb://localhost:27017/tidepodtrickshots", {useNewUrlParser: true});
-
-//seeding the database
-var seedDB = require("./seed");
-seedDB();
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/tidepodtrickshots"
+mongoose.connect(url, {useNewUrlParser: true});
 
 //connect the stylesheet
 app.use(express.static(__dirname + "/public")); // __dirname gives us the directory path we are in
@@ -59,6 +55,7 @@ app.use(bloopersRoutes);
 app.use(indexRoutes);
 
 // Starts the server
-app.listen(3000, function(){
-    console.log("Tidepod Trickshots server is running");
+var port = process.env.PORT || 3000
+app.listen(port, function(){
+    console.log("Tidepod Trickshots server is running on " + port);
 });
